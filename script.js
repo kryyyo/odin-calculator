a7ebc66be065942f77233ecab7a149fa6f323102
Collene De Silva <collene.desilva@gmail.com>
2022-03-19 03:08:04 +0800

    Add 2 screens for calculator, for last and current input

---
+++ script.js
@@ -0,0 +1,5 @@
+/* Element Selectors */
+    const calcEyes = document.querySelector('.calc-eyes')
+
+    // For the clicking animation and sound of buttons
+    const calcButtons = document.querySelectorAll('.calc-buttons')
