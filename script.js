c2b7e7bac40b53c7b976c7b88a648bd855f166a3
Collene De Silva <collene.desilva@gmail.com>
2022-03-19 03:23:33 +0800

    Add elements and listeners, and test event

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 /* Element Selectors */
     const calcEyes = document.querySelector('.calc-eyes')
+    const calcScreenLast = document.querySelector('#calc-screen-last')
+    const calcScreenCurrent = document.querySelector('#calc-screen-current')
+
+    const numberButtons = document.querySelectorAll('.btn-num')
+    const operatorButtons = document.querySelectorAll('.btn-operator')
+    const clearButton = document.querySelector('#btn-clear')
+    const deleteButton = document.querySelector('#btn-delete')
+    const decimalButton = document.querySelector('#btn-decimal')
+    const equalButton = document.querySelector('#btn-equals')
 
     // For the clicking animation and sound of buttons
     const calcButtons = document.querySelectorAll('.calc-buttons')
+
+/* Event Listeners */
+    //test only first
+    numberButtons.forEach(button => button.addEventListener('click', testEvent))
+    operatorButtons.forEach(button => button.addEventListener('click', testEvent))
+    clearButton.addEventListener('click', testEvent)
+    deleteButton.addEventListener('click', testEvent)
+    decimalButton.addEventListener('click', testEvent)
+    equalButton.addEventListener('click', testEvent)
+
+
+/* Function Tester */
+function testEvent(e) {
+    console.log(e)
+}
